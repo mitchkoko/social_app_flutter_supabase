e99f6d1451fabc9c8c611ccbcfe56180d040ad8c
Chris Southam <chris@cjsoutham.com>
2024-11-18 14:03:43 +0000

    Move Supabase credentials to environment variables

--- lib/keys.dart
+++
@@ -1,3 +0,0 @@
-// get these from your supabase project
-const String supabaseUrl = 'your-supabase-url';
-const String supabaseAnonKey = 'your-supabase-anon-key';
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:social_media_app/features/auth/auth_gate.dart';
-import 'package:social_media_app/keys.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // load env
+  await dotenv.load();
+
+  // initialize supabase
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabaseAnonKey = dotenv.env['SUPABASE_KEY'] ?? '';
+
   // supabase setup
   await Supabase.initialize(
     url: supabaseUrl,
